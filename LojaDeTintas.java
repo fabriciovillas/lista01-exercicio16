ecadbdec1ff6085d5688aad81decdc80e4c4da89
fabriciovillas <fabriciovbmf@hotmail.com>
2023-04-04 15:14:12 -0300

    Nova mudança do nome

--- LojaDeTintas.java
+++ LojaDeTintas.java
@@ -4,15 +4,15 @@ public class LojaDeTintas {
     public static void main(String[] args) {
        int area;
        double litros;
-       double quantidadedelatas; 
+       double quantidadeDeLatas; 
        double valorFinal;
        Scanner input;
        input = new Scanner(System.in);
        area = input.nextInt();
        litros = area / 3;
-       quantidadedelatas = Math.ceil (litros / 18);
-       valorFinal = quantidadedelatas * 80;
-       System.out.println(quantidadedelatas);
+       quantidadeDeLatas = Math.ceil (litros / 18);
+       valorFinal = quantidadeDeLatas * 80;
+       System.out.println(quantidadeDeLatas);
        System.out.println("R$" + valorFinal);
     }
 }
